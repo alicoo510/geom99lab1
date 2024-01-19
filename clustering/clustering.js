d406107af1a574b30c2f49a10aeb77cb3810aa36
alicoo510 <146375997+alicoo510@users.noreply.github.com>
2024-01-19 14:46:21 -0500

    zoom

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
+    zoom: 6,
     center: { lat:  47.84232762933268, lng:-83.40138419396823 },
   });
   // Create an array of alphabetical characters used to label the markers.
